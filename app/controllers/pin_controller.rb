6b444eb9a35b0d963448bed450481c5321e7a2e7
Ruth <marinruth3@gmail.com>
2019-07-24 19:56:55 -0400

    adsa

--- app/controllers/pin_controller.rb
+++ app/controllers/pin_controller.rb
@@ -1,2 +1,7 @@
 class PinController < ApplicationController
+  before_action :set_pine, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pines = Pine.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :pines
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'pin#index'
+  root 'pines#index'
 end
